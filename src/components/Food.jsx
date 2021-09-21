970b9867b7b36ffb9a62da3dd4dad7dd1e7d45f6
johnTran <mrjohntran123@gmail.com>
2021-09-21 15:16:42 -0700

    add a logo for the app

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -3,7 +3,7 @@ import { baseURL, config } from "../services";
 import { Link } from 'react-router-dom';
 
 function Food(props){
-    const {name, side, drink, rice, protein, number } = props.food.fields
+    const {name, side, rice, protein }= props.food.fields
 
     const editFood = async () => {
         const foodURL = `${baseURL}/${props.food.id}`;
@@ -15,10 +15,8 @@ function Food(props){
         <div className="food">
             <h3>{name}</h3>
             <h3>{side}</h3>
-            <h3>{drink}</h3>
             <h3>{rice}</h3>
             <h3>{protein}</h3>
-            <h3>{number}/5</h3>
             <button onClick = {editFood}>Edit/Delete</button>
             <Link to={`/edit${props.food.id}`}>
             <button>Edit Order</button></Link>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom';
+// import { useState } from 'react';
 
 function Nav() {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+            {/* <Link to='/' className='navbar-logo'>
+                Com Tam <i class="fa-solid fa-receipt"></i>
+            </Link> */}
+            <Link to='/' className='navbar-logo'> Home <i className = 'fa-solid fa-receipt' />
+            </Link>
+            
             <Link to='/'>Menu</Link>
+            <Link to='/'>Contact</Link>
+            </div>
         </nav>
     )
 }
