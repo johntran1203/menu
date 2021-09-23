780965716140455d4f72c3ed4cba4a64ee0e8d21
johnTran <mrjohntran123@gmail.com>
2021-09-23 13:07:01 -0700

    add css on the homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,20 @@
 
 import React from 'react';
-import './Footer.css';
+import '../App.css';
 
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-container'>
-    
-      
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          
-          <small class='website-rights'>RICE © 2021</small>
-          <div class='social-icons'>
+          <h2>RICE © 2021</h2>
             <Link
               class='social-icon-link github'
               to='https://github.com/johntran1203/menu'
               target='_blank'
-              aria-label='Github'
-            >
+              aria-label='Github' >
               <i class='fab fa-github' />
             </Link>
-          
-          
-           
-          </div>
-        </div>
-      </section>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,29 @@
 import { Link } from 'react-router-dom';
+import img from '../images/com.jpg'
+import '../App.css';
 
 function Home() {
     return (
-        <div className= 'container'>
+        <div>
             <h1>Simple Com Tam</h1>
+            <div className='home-container'>
+            <div className='col'>
+            <img src={img} alt="rice" />
+            </div>
+            <div className='col'>
             <h3>ABOUT US</h3>
-            <p>When going to an authentic vietnamese restaurant it could confusing and stressful experience. As soon as you sit down the waiter gives you a menu and ask what you want right away. The menu is in vietnamese with a little bit of english. There's no picture of what you want to order. With our App user can pick there food like chiptole style. No need to deal with a server and there will be pictures of what you want to order. </p>
+            <p>When going to an authentic vietnamese restaurant it could confusing and stressful experience. As soon as you sit down the waiter gives you a menu and ask what you want right away. The menu is in vietnamese with a little bit of english. There's no picture of what you want to order. With our App user can pick there food like chiptole style. </p>
             <div className='home-btn'>
                 <Link to='/new'>
                     <button className='btns'>ORDER HERE</button>
-                   
                 </Link>
             
             </div>
+            </div>
+            </div>
+           
+            
+    
             
         </div>
         
