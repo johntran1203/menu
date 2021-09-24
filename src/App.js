37313d76ed382f4888320933da1a3260ff893993
johnTran <mrjohntran123@gmail.com>
2021-09-24 16:38:36 -0700

    ready for presentation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Form from "./components/Form";
 import { baseURL, config } from "./services";
+import FilterFavorite from "./components/FilterFavorite";
 import "./App.css";
 
 function App() {
@@ -15,11 +16,18 @@ function App() {
   const [toggleFetch, setToggleFetch] = useState(false);
   const [search, setSearch] = useState("");
 
+ 
+  const filterFavorite = foods.map((food) => <FilterFavorite key ={food.id} food ={food}/>)
+  
+  
   useEffect(() => {
     const getMenu = async () => {
       const response = await axios.get(baseURL, config);
 
       setFood(response.data.records);
+      const red = response.data.records;
+      
+    
     };
     getMenu();
   }, [toggleFetch]);
@@ -33,11 +41,11 @@ function App() {
       <Route exact path="/menu">
 
         <main>
-          <input type='text' placeholder = 'search' onChange ={(e) => {setSearch(e.target.value)}} />
-          
-          {foods.map((food) => (
+          {/* <input type='text' placeholder = 'search' onChange ={(e) => {setSearch(e.target.value)}} /> */}
+          {foods.map((food, key) => (
             <Food key={food.id} food={food} setToggleFetch={setToggleFetch} />
           ))}
+        
         </main>
       </Route>
       <Route path="/home">
@@ -49,6 +57,11 @@ function App() {
       <Route path="/edit/:id">
         <Form foods={foods} setToggleFetch={setToggleFetch} />
       </Route>
+      <Route path="/filterFavorite">
+        <h1>Hello world</h1>
+        <input type='text' placeholder = 'search' onChange ={(e) => {setSearch(e.target.value)}} />
+          {filterFavorite}
+      </Route>
       <Route path="/contact">
         <Contact />
       </Route>
@@ -59,3 +72,6 @@ function App() {
 
 export default App;
 
+// {foods.map((val, key) => {
+//   return
+// })}
---
+++ src/components/FilterFavorite.jsx
@@ -0,0 +1,13 @@
+
+
+function FilterFavorite(props) {
+    return (
+        <div>
+            {/* <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p> */}
+            <h2>Test</h2>
+        </div>
+    )
+}
+
+export default FilterFavorite;
